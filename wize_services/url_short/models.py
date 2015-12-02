23857c6b0c90da8c9997cec88c993bddc6ee5a3b
Javier Sanchez <reivaj0705@gmail.com>
2015-12-02 16:04:44 +0000

    Create Url model for short url app

---
+++ wize_services/url_short/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+
+
+class UrlShort(models.Model):
+
+    long_url = models.CharField(
+        _('Long url'),
+        max_length=200,
+        help_text=_('Insert Url to convert')
+    )
+    short_url = models.CharField(
+        _('Short url'),
+        max_length=200,
+        help_text=_('Insert Url to convert')
+    )
+
+    def __unicode__(self):
+        return '{long_url} {short_url}'.format(
+            long_url=self.long_url,
+            short_url=self.short_url)
